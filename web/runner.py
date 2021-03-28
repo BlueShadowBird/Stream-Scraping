af0b452ec0d52c57f4132f3f3ad2504fab26d960
Dede Kurniawan <blue_shadow_bird@yahoo.co.id>
2021-03-28 14:02:24 +0700

    add web function with flask

--- web/runner.py
+++ web/runner.py
@@ -1,8 +1,18 @@
-from web.steam_scaper import SteamScraper
+from flask import Flask, render_template
+
+from web.steam_scraper import SteamScraper
 
 url = 'https://store.steampowered.com/'
 
 scraper = SteamScraper()
 scraper.scrape()
-for data in scraper.get():
-    print(data)
+top_seller_datas = scraper.get()
+
+app = Flask(__name__)
+
+@app.route('/')
+def test():
+    return render_template('top_seller.html', datas=top_seller_datas)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- web/steam_scraper.py
+++ web/steam_scraper.py
@@ -34,25 +34,46 @@ class SteamScraper:
                 'class': 'search_result_row ds_collapse_flag'})
 
             for seller in sellers:
+                url = seller['href']
                 title = seller.find('span', attrs={'class': 'title'}).text
                 pic_url = seller.find('div', attrs={'class': 'col search_capsule'}).find('img')['src']
                 released = seller.find('div', attrs={'class': 'col search_released responsive_secondrow'}).text
                 price_div = seller.find('div', attrs={'class': 'col search_price responsive_secondrow'})
-                if None == price_div:
-                    price_div = seller.find('div', attrs={'class': 'col search_price_discount_combined responsive_secondrow'})
-                    diskon_price = price_div.find('div', attrs={'class': 'col search_price discounted responsive_secondrow'}).contents[3]
-                    diskon = price_div.find('div', attrs={'class': 'col search_discount responsive_secondrow'}).text.strip()
-                    original_price = price_div.find('div', attrs={'class': 'col search_price discounted responsive_secondrow'})\
-                        .find('span').text.strip()
-                    price = {'original_price': original_price, 'diskon': diskon, 'diskon_price': diskon_price}
+
+                discount = ''
+                original_price = ''
+                if price_div is None:
+                    price_div = seller.find('div', attrs={
+                        'class': 'col search_price_discount_combined responsive_secondrow'})
+                    price = price_div.find('div', attrs={
+                        'class': 'col search_price discounted responsive_secondrow'}).contents[3]
+                    discount = price_div.find('div', attrs={
+                        'class': 'col search_discount responsive_secondrow'}).text.strip()
+                    original_price = price_div.find('div', attrs={
+                        'class': 'col search_price discounted responsive_secondrow'}).find('span').text.strip()
                 else:
                     price = price_div.text.strip()
 
-                platform_datas = seller.find('div', attrs={'class': 'col search_name ellipsis'}).find('p').find_all('span')
+                platform_datas = seller.find('div', attrs={'class': 'col search_name ellipsis'}).find('p').find_all(
+                    'span')
                 platforms = []
                 for platform_data in platform_datas:
-                    platforms.append(platform_data['class'])
-                self.result.append({'title': title, 'pic_url': pic_url, 'released': released, 'price': price, 'platforms': platforms})
+                    platform_class = platform_data['class']
+                    if 'win' in platform_class:
+                        platforms.append('Windows OS')
+                    elif 'linux' in platform_class:
+                        platforms.append('Linux OS')
+                    elif 'mac' in platform_class:
+                        platforms.append('Mac OS')
+                    elif 'vr_supported' in platform_class:
+                        platforms.append('VR Supported')
+                    elif 'vr_required' in platform_class:
+                        platforms.append('VR Only')
+                    else:
+                        platforms.append('Not Supported: {}'.format(platform_class))
+                self.result.append(
+                    {'title': title, 'url': url, 'pic_url': pic_url, 'released': released, 'price': price,
+                     'discount': discount, 'original_price': original_price, 'platforms': platforms})
 
     def get(self):
         return self.result
